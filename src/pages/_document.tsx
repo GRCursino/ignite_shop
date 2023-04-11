06023d183b290a94d9e6c0f954407ec4a7ffd6f8
GRCursino <guiiix93@gmail.com>
2023-04-10 22:44:14 -0300

    Initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        
+        {/*Configuracao pro Server Side Rendering pegar o CSS e renderizar junto do HTML, mesmo com javascript do navegador desligado*/}
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      </Head>  
       <body>
         <Main />
         <NextScript />
